5e9827cec8d4da2fbcc53e0b0e73d0af167bb786
Felix Nuworsu <nuworsufelix49@gmail.com>
2025-06-25 11:03:08 -0700

    feat: local-strategy for user auth  and react routes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import LoginPage from './components/SignInUp/LoginPage.jsx'
 import LandingPage from './components/LandingPage/LandingPage.jsx'
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-    <LoginPage></LoginPage>
+      <Routes>
+        <Route path='/' element={<LandingPage></LandingPage>}></Route>
+        <Route path='/login' element={<LoginPage></LoginPage>}></Route>
+      </Routes>
     </>
   )
 }
