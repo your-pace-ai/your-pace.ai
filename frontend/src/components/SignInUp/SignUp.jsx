55ff90fad30e70a83d44e45f02e8ffabaf661268
Felix Nuworsu <nuworsufelix49@gmail.com>
2025-06-25 11:43:03 -0700

    refactor: included reactrouter routes for sign-up page

--- frontend/src/components/SignInUp/SignUp.jsx
+++ frontend/src/components/SignInUp/SignUp.jsx
@@ -1,5 +1,5 @@
 import "./SignUp.css"
-import { useState } from "react";
+import { useState } from "react"
 
 export const SignUp = () => {
     const [showPassword, setshowPassword] = useState(false);
@@ -29,7 +29,7 @@ export const SignUp = () => {
               <p className="login-form-subtitle">Let's start your learning journey.</p>
               <button className="login-google-btn">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Google_Favicon_2025.svg/250px-Google_Favicon_2025.svg.png" alt="G" className="google-icon" />
-                SignUp with Google
+                Signup with Google
               </button>
               <div className="login-or-row">
                 <div className="login-or-line" />
@@ -37,28 +37,28 @@ export const SignUp = () => {
                 <div className="login-or-line" />
               </div>
               <form>
-                {/* first name */}
+
                 <input
                   className="login-input-first-name"
                   type="text"
                   placeholder="Enter your first name"
                   autoComplete="username"
                 />
-                {/* last name */}
+
                 <input
                   className="login-input-last-name"
                   type="text"
                   placeholder="Enter your last name"
                   autoComplete="username"
                 />
-                {/* email */}
+
                 <input
                   className="login-input-email"
                   type="email"
                   placeholder="Enter your email"
                   autoComplete="username"
                 />
-                {/* enter password */}
+
                 <div className="login-password-row">
                   <input
                     className="login-input-password"
