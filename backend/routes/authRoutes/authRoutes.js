a34b35395d89808b32a35558a03f08f8081d8efd
Felix Nuworsu <nuworsufelix49@gmail.com>
2025-07-01 14:28:24 -0700

    fix: req.user sending sensitive info

--- backend/routes/authRoutes/authRoutes.js
+++ backend/routes/authRoutes/authRoutes.js
@@ -26,7 +26,7 @@ router.post("/api/local-auth/signup", validateLocalSignUp, async (req, res) => {
 })
 
 router.post("/api/local-auth/login", passport.authenticate("local"), async (req, res) => {
-    res.json(req.user)
+    res.json(req.user.id)
 })
 
 router.post("/api/local-auth/logout", (req, res) => {
