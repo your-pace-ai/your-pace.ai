774f364438b201d245963d31800da6883b5bf7e7
Felix Nuworsu <nuworsufelix49@gmail.com>
2025-07-01 15:52:43 -0700

    fix: using dom and manually setting modal

--- frontend/src/components/Modal/CreateLearningHubModal.jsx
+++ frontend/src/components/Modal/CreateLearningHubModal.jsx
@@ -1,38 +1,32 @@
 import { createLearningHub } from "../../api/api"
-import "./Modal.css"
+import "./CreateLearningHubModal.css"
 import { useState, useEffect } from "react"
 
-export const Modal = () => {
+export const Modal = ({visible, onClose}) => {
     const [LearningHub, setLearningHub] = useState("")
-    const [modal, setModal] = useState("")
 
-    useEffect(() => {
-        setModal(document.getElementsByClassName("modal")[0])
-    })
-
-    const closeModal = () => {
-        modal.style.display = "none"
-    }
+    if (!visible) return null
 
     const handleSubmit = async(e) => {
         e.preventDefault()
         await createLearningHub(LearningHub)
         closeModal()
+        onClose()
     }
 
     return (
         <>
-            <div className="modal">
-                <div className="modal-content">
-                    <span className="close" onClick={closeModal}>&times;</span>
-                    <h3 className="modal-header">Ready to create your learning hub?</h3>
-                    <form className="modal-form" onSubmit={handleSubmit}>
+            <div className="create-learning-hub-modal" onClick={onClose}>
+                <div className="create-learning-hub-modal-content" onClick={e =>e.stopPropagation()}>
+                    <span className="close" onClick={onClose}>&times;</span>
+                    <h3 className="create-learning-hub-modal-header">Ready to create your learning hub?</h3>
+                    <form className="create-learning-hub-modal-form" onSubmit={handleSubmit}>
                             <input type="text"
                                    value={LearningHub}
                                    onChange={e => setLearningHub(e.target.value)}
                                    placeholder="Enter a name for your learning hub"
                             />
-                        <button type="submit" className="modal-button">Create</button>
+                        <button type="submit" className="create-learning-hub-modal-button">Create</button>
                     </form>
                 </div>
             </div>
--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,25 +1,20 @@
 import { useEffect, useState } from "react"
 import "./Sidebar.css"
 import { useNavigate } from "react-router-dom"
-import { currentUser, logout } from "../../api/api"
+import { logout } from "../../api/api"
 
-export const Sidebar = () => {
+export const Sidebar = ({onOpenModal}) => {
     const navigate = useNavigate()
     const [modal, setModal] = useState("")
 
 
     useEffect(() => {
-        setModal(document.getElementsByClassName("modal")[0])
+        setModal(document.getElementsByClassName("create-learning-hub-modal")[0])
     }, [])
 
     const signOut = async () => {
         await logout()
         navigate("/login")
-    }
-
-    const handleButtonClick = (e) => {
-        e.preventDefault()
-        modal.style.display = "block"
     }
 
     return (
@@ -35,7 +30,7 @@ export const Sidebar = () => {
                 <div className="sidebar-content">
                     <button className="sidebar-btn primary">+ Add Content</button>
                     <button className="sidebar-btn">Recent</button>
-                    <button className="sidebar-btn" onClick={handleButtonClick}>+ Create Hub</button>
+                    <button className="sidebar-btn" onClick={onOpenModal}>+ Create Hub</button>
                     <button className="sidebar-btn">Community</button>
                     <button className="sidebar-btn sign-out" onClick={signOut}>Sign Out</button>
                 </div>
