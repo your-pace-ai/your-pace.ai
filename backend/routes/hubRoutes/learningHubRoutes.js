5dfa521e76b0d308fbc23a26694a60f5d6bb3c90
Felix Nuworsu <nuworsufelix49@gmail.com>
2025-07-04 12:33:28 -0700

    refactor: used try catch in learning routes for proper erro handling

--- backend/routes/hubRoutes/learningHubRoutes.js
+++ backend/routes/hubRoutes/learningHubRoutes.js
@@ -5,6 +5,7 @@ const prisma = new PrismaClient()
 const router = express.Router()
 
 router.post("/api/learning-hub/create",isAuthenticated, async (req, res) => {
+  try {
     const userId = req.user.id
     const { body : { title }} = req
     const learningHub = await prisma.learningHub.create({
@@ -18,31 +19,67 @@ router.post("/api/learning-hub/create",isAuthenticated, async (req, res) => {
         }
     })
     res.json(learningHub)
+  } catch (error) {
+    res.status(500).json({error: "Failed to create learning hub"})
+  }
 })
 
 router.get("/api/learning-hub", isAuthenticated, async (req, res) => {
-  const userId = req.user.id
+  try {
+    const userId = req.user.id
 
-  const user = await prisma.user.findUnique({
-    where: {
-      id: userId
-    },
-    include: {
-      learningHub: true
-    },
-  })
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId
+      },
+      include: {
+        learningHub: true
+      },
+    })
 
-  res.json(user.learningHub)
+    res.json(user.learningHub)
+  } catch (error) {
+    res.status(500).json({error: "Failed to get learning hub"})
+  }
 })
 
 router.delete("/api/learning-hub/delete", isAuthenticated, async (req, res) => {
-    const { body: { learningHubId } } = req
-    const deletedLearningHub = await prisma.learningHub.delete({
+    try {
+      const { body: { learningHubId } } = req
+      const userId = req.user.id
+      const deletedLearningHub = await prisma.learningHub.delete({
         where: {
-            id: learningHubId
+            id: learningHubId,
+            userId: userId
         }
+      })
+      if (!deletedLearningHub) return res.status(404).json({error: "Learning hub not found"})
+      res.json(deletedLearningHub)
+    } catch (error) {
+      res.status(500).json({error: "Failed to delete learning hub"})
+    }
+})
+
+router.get("/api/learning-hub/:id/subhubs", isAuthenticated, async (req, res) => {
+  try {
+    const learningHubId = parsedInt(req.params.id)
+    const userId = req.user.id
+
+    const learningHub = await prisma.learningHub.findFirst({
+      where: {
+        id: learningHubId,
+        userId: userId
+      },
+      include: {
+        subHub: true
+      }
     })
-    res.json(deletedLearningHub)
+    if (!learningHub) return res.status(404).json({error: "Learning hub not found"})
+    res.json(learningHub.subHub)
+
+  } catch (error) {
+    res.status(500).json({error: "Failed to get subhubs"})
+  }
 })
 
 module.exports = router
